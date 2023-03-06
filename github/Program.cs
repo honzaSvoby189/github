1e7dff6bbece6ca4acc2068b242a14bb22350368
jan.svoboda <jan.svoboda@VT1-16>
2023-03-06 10:42:10 +0100

    Přidejte soubory projektu.

---
+++ github/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("čau more github, já jsem jena , a chodím na educhme v mostě, bavímě :");
+Console.WriteLine("-----------------------------------");
+string[] coMěBaví = { "fotbal", "hraní fonka", "swonboard", "cestování", };
+
+foreach (var item in coMěBaví)
+{
+    Console.WriteLine(item);
+}
